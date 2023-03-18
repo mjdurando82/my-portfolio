185060a68091c28f0bfb262af9c7e701702ccff6
Michael Durando <mjdurando82@yahoo.com>
2023-03-18 18:48:51 -0400

    updated portfolio

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     email: 'durandomike@gmail.com',
     availability: 'Open for work',
     brand:
-      'I am a Software Engineer who finds success by creating customized and efficient solutions to help improve a project or system through JavaScript, HTML, and CSS. I am driven by being detail and solution oriented. I thrive in a team environment to build user friendly products. Similarly to my experience in the public service sector, I like to know my work has a positive impact on others.'
+      'I am a Software Engineer who finds success by creating customized and efficient solutions to help improve a project or system. I am driven by being detail and solution oriented. I thrive in a team environment to build user friendly products. Similarly to my experience in the public service sector, I like to know my work has a positive impact on others.'
   }
 
   return (
